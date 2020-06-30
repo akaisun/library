f8035c93e27ecd73eb969c38a6b2d6a5c7b47a27
sun <kis0619@gmail.com>
2020-06-30 23:05:22 +0900

    no message

--- app/src/main/java/com/akaisun/library/CustomRecyclerView.kt
+++ app/src/main/java/com/akaisun/library/CustomRecyclerView.kt
@@ -2,31 +2,33 @@ package com.akaisun.library
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SimpleItemAnimator
 
-class CustomRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : RecyclerView(context, attrs) {
+class CustomRecyclerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : RecyclerView(context, attrs, defStyle) {
 
     init {
-        init()
+        init(attrs, defStyle)
     }
 
-    private fun init() {
+    private fun init(attrs: AttributeSet?, defStyle: Int) {
         setHasFixedSize(true)
-        setLineColor(context.getColor(R.color.list_decoration_color))
 
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomRecyclerView, defStyle, 0)
+        val orientation = typedArray.getInt(R.styleable.CustomRecyclerView_android_orientation, LinearLayoutManager.VERTICAL)
+        val itemDecoration = DividerItemDecoration(context, orientation).also {
+            it.drawable?.setTint(context.getColor(R.color.list_decoration_color))
+        }
+        addItemDecoration(itemDecoration)
         itemAnimator?.let {
             if(it is SimpleItemAnimator) it.supportsChangeAnimations = false
         }
     }
-
-    fun setLineColor(color: Int) {
-        val itemDecoration = DividerItemDecoration(context, LinearLayoutManager.VERTICAL).also {
-            it.drawable?.setTint(color)
-        }
-        addItemDecoration(itemDecoration)
-    }
-
 }
